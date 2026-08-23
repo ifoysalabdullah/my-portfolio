4f100f5e54f35060424cb2c49c99a0ba8d63439a
Foysal Abdullah <foysal@Foysals-MacBook-Air.local>
2026-08-23 23:23:42 +0600

    Updated homepage video and fixed clouds

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,4 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://foysalabdullah.com', // Add your actual website URL here
   integrations: [sitemap()],
-});
+})
